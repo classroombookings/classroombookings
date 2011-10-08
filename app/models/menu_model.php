d04f6cd4c8c97fd701e964e788046325205c0f1d
Craig A Rodway <craig.rodway@gmail.com>
2011-10-08 18:49:47 +0100

    Updated language file for groups & permissions

--- app/models/menu_model.php
+++ app/models/menu_model.php
@@ -54,8 +54,8 @@ class Menu_model extends CI_Model
 		$subnav[] = array('configure/settings', lang('GENERAL_SETTINGS'), 'configure', 'configure-settings');
 		$subnav[] = array('authentication', lang('AUTHENTICATION'), 'configure', 'configure-authentication');
 		$subnav[] = array('users', lang('USERS'), 'users', 'configure-users');
-		$subnav[] = array('groups', lang('USER_GROUPS'), 'groups', 'configure-groups');
-		$subnav[] = array('permissions', lang('GROUP_PERMISSIONS'), 'permissions', 'configure-permissions');
+		$subnav[] = array('groups', lang('GROUPS'), 'groups', 'configure-groups');
+		$subnav[] = array('permissions', lang('PERMISSIONS'), 'permissions', 'configure-permissions');
 		$subnav[] = array('departments', lang('DEPARTMENTS'), 'departments', 'configure-departments');
 		$subnav[] = array('rooms/manage', lang('ROOMS'), 'rooms', 'configure-rooms');
 		$subnav[] = array('academic/years', lang('ACADEMIC_YEARS'), 'years', 'configure-years');
