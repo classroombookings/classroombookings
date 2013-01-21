4f56c6f44531c96f1db45f6da9642e91ad5c0421
Craig Rodway <craig.rodway@gmail.com>
2013-01-21 21:12:19 +0000

    Custom configuration file for core system configurations/lookups.

---
+++ src/application/config/crbs.php
@@ -0,0 +1,9 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['permission_entities'] = array(
+	'E' => 'Everyone',
+	'D' => 'Department',
+	'G' => 'Group',
+	'U' => 'User',
+	'R' => 'Role',
+);
