8da1254b44a6b641c41306ab193b6f6414a84816
Craig A Rodway <craig.rodway@gmail.com>
2018-11-01 22:51:58 +0000

    Updated form access on profile edit

--- application/views/profile/profile_edit.php
+++ application/views/profile/profile_edit.php
@@ -1,67 +1,61 @@
 <?php
-if(!isset($user_id)){ $user_id = $this->session->userdata('user_id'); }
 
-echo form_open('profile/save', array('class' => 'cssform', 'id' => 'profile_edit'), array('user_id' => $user_id) );
+echo form_open('profile/save', array('class' => 'cssform', 'id' => 'profile_edit'));
 
-$t = 1;
 ?>
 
 
-<fieldset><legend accesskey="U" tabindex="<?php echo $t; $t++; ?>">User Information</legend>
+<fieldset>
+
+	<legend accesskey="U" tabindex="<?php tab_index() ?>">User Information</legend>
+
+	<p>
+	  <label for="email" class="required">Email address</label>
+	  <?php
+		$email = set_value('email', $user->email);
+		echo form_input(array(
+			'name' => 'email',
+			'id' => 'email',
+			'size' => '35',
+			'maxlength' => '255',
+			'tabindex' =>tab_index(),
+			'value' => $email,
+		));
+		?>
+	</p>
+	<?php echo form_error('email'); ?>
 
 
-<p>
-  <label for="email" class="required">Email address</label>
-  <?php
-	$email = @field($this->validation->email, $user->email);
-	echo form_input(array(
-		'name' => 'email',
-		'id' => 'email',
-		'size' => '35',
-		'maxlength' => '255',
-		'tabindex' => $t,
-		'value' => $email,
-	));
-	$t++;
-	?>
-</p>
-<?php echo @field($this->validation->email_error) ?>
+	<p>
+	  <label for="password1">Password</label>
+	  <?php
+		echo form_password(array(
+			'name' => 'password1',
+			'id' => 'password1',
+			'size' => '20',
+			'maxlength' => '40',
+			'tabindex' => tab_index(),
+			'value' => '',
+		));
+		?>
+	</p>
+	<?php echo form_error('password1'); ?>
 
 
-<p>
-  <label for="password1">Password</label>
-  <?php
-	#$password1 = @field($this->validation->email, $user->email);
-	echo form_password(array(
-		'name' => 'password1',
-		'id' => 'password1',
-		'size' => '20',
-		'maxlength' => '40',
-		'tabindex' => $t,
-		'value' => '',
-	));
-	$t++;
-	?>
-</p>
-<?php echo @field($this->validation->password1_error) ?>
-
-
-<p>
-  <label for="password2">Password (again)</label>
-  <?php
-	#$password1 = @field($this->validation->email, $user->email);
-	echo form_password(array(
-		'name' => 'password2',
-		'id' => 'password2',
-		'size' => '20',
-		'maxlength' => '40',
-		'tabindex' => $t,
-		'value' => '',
-	));
-	$t++;
-	?>
-</p>
-<?php echo @field($this->validation->password2_error) ?>
+	<p>
+	  <label for="password2">Password (again)</label>
+	  <?php
+		echo form_password(array(
+			'name' => 'password2',
+			'id' => 'password2',
+			'size' => '20',
+			'maxlength' => '40',
+			'tabindex' => tab_index(),
+			'value' => '',
+		));
+		?>
+	</p>
+	<?php echo form_error('password2'); ?>
 
 
 </fieldset>
@@ -70,84 +64,82 @@ $t = 1;
 <fieldset>
 
 
-<p>
-  <label for="firstname">First name</label>
-  <?php
-	$firstname = @field($this->validation->firstname, $user->firstname);
-	echo form_input(array(
-		'name' => 'firstname',
-		'id' => 'firstname',
-		'size' => '20',
-		'maxlength' => '20',
-		'tabindex' => $t,
-		'value' => $firstname,
-	));
-	$t++;
-	?>
-</p>
-<?php echo @field($this->validation->firstname_error) ?>
+	<p>
+	  <label for="firstname">First name</label>
+	  <?php
+		$firstname = set_value('firstname', $user->firstname);
+		echo form_input(array(
+			'name' => 'firstname',
+			'id' => 'firstname',
+			'size' => '20',
+			'maxlength' => '20',
+			'tabindex' => tab_index(),
+			'value' => $firstname,
+		));
+		?>
+	</p>
+	<?php echo form_error('firstname'); ?>
 
 
-<p>
-  <label for="lastname">Last name</label>
-  <?php
-	$lastname = @field($this->validation->lastname, $user->lastname);
-	echo form_input(array(
-		'name' => 'lastname',
-		'id' => 'lastname',
-		'size' => '20',
-		'maxlength' => '20',
-		'tabindex' => $t,
-		'value' => $lastname,
-	));
-	$t++;
-	?>
-</p>
-<?php echo @field($this->validation->lastname_error) ?>
+	<p>
+	  <label for="lastname">Last name</label>
+	  <?php
+		$lastname = set_value('lastname', $user->lastname);
+		echo form_input(array(
+			'name' => 'lastname',
+			'id' => 'lastname',
+			'size' => '20',
+			'maxlength' => '20',
+			'tabindex' => tab_index(),
+			'value' => $lastname,
+		));
+		?>
+	</p>
+	<?php echo form_error('lastname'); ?>
 
 
-<p>
-  <label for="displayname">Display name</label>
-  <?php
-	$displayname = @field($this->validation->displayname, $user->displayname);
-	echo form_input(array(
-		'name' => 'displayname',
-		'id' => 'displayname',
-		'size' => '20',
-		'maxlength' => '20',
-		'tabindex' => $t,
-		'value' => $displayname,
-	));
-	$t++;
-	?>
-</p>
-<?php echo @field($this->validation->displayname_error) ?>
+	<p>
+	  <label for="displayname">Display name</label>
+	  <?php
+		$displayname = set_value('displayname', $user->displayname);
+		echo form_input(array(
+			'name' => 'displayname',
+			'id' => 'displayname',
+			'size' => '20',
+			'maxlength' => '20',
+			'tabindex' => tab_index(),
+			'value' => $displayname,
+		));
+		?>
+	</p>
+	<?php echo form_error('displayname'); ?>
 
 
-<p>
-  <label for="ext">Extension</label>
-  <?php
-	$ext = @field($this->validation->ext, $user->ext);
-	echo form_input(array(
-		'name' => 'ext',
-		'id' => 'ext',
-		'size' => '10',
-		'maxlength' => '10',
-		'tabindex' => $t,
-		'value' => $ext,
-	));
-	$t++;
-	?>
-</p>
-<?php echo @field($this->validation->ext_error) ?>
+	<p>
+	  <label for="ext">Extension</label>
+	  <?php
+		$ext = set_value('ext', $user->ext);
+		echo form_input(array(
+			'name' => 'ext',
+			'id' => 'ext',
+			'size' => '10',
+			'maxlength' => '10',
+			'tabindex' => tab_index(),
+			'value' => $ext,
+		));
+		?>
+	</p>
+	<?php echo form_error('ext'); ?>
 
 
 </fieldset>
 
 
 <?php
-$submit['submit'] = array('Save', $t);
-$submit['cancel'] = array('Cancel', $t+1, 'profile');
-$this->load->view('partials/submit', $submit);
+$this->load->view('partials/submit', array(
+	'submit' => array('Save', tab_index()),
+	'cancel' => array('Cancel', tab_index(), 'profile'),
+));
+
 echo form_close();
 ?>
