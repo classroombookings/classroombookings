9fc52c5fbacff2bec0e4a17d2b60083965602420
Craig A Rodway <craig.rodway@gmail.com>
2018-11-08 21:50:14 +0000

    Whitespace

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -8,7 +8,7 @@ var dayArrayLong = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursd
 var monthArrayShort = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
 var monthArrayMed = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
 var monthArrayLong = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
- 
+
 // these variables define the date formatting we're expecting and outputting.
 // If you want to use a different format by default, change the defaultDateSeparator
 // and defaultDateFormat variables either here or on your HTML page.
@@ -20,27 +20,27 @@ var dateFormat = defaultDateFormat;
 function displayDatePicker(dateFieldName, displayBelowThisObject, dtFormat, dtSep)
 {
   var targetDateField = document.getElementsByName (dateFieldName).item(0);
- 
+
   // if we weren't told what node to display the datepicker beneath, just display it
   // beneath the date field we're updating
   if (!displayBelowThisObject)
     displayBelowThisObject = targetDateField;
- 
+
   // if a date separator character was given, update the dateSeparator variable
   if (dtSep)
     dateSeparator = dtSep;
   else
     dateSeparator = defaultDateSeparator;
- 
+
   // if a date format was given, update the dateFormat variable
   if (dtFormat)
     dateFormat = dtFormat;
   else
     dateFormat = defaultDateFormat;
- 
+
   var x = displayBelowThisObject.offsetLeft;
   var y = displayBelowThisObject.offsetTop + displayBelowThisObject.offsetHeight ;
- 
+
   // deal with elements inside tables and such
   var parent = displayBelowThisObject;
   while (parent.offsetParent) {
@@ -48,7 +48,7 @@ function displayDatePicker(dateFieldName, displayBelowThisObject, dtFormat, dtSe
     x += parent.offsetLeft;
     y += parent.offsetTop ;
   }
- 
+
   drawDatePicker(targetDateField, x, y);
 }
 
@@ -56,7 +56,7 @@ function displayDatePicker(dateFieldName, displayBelowThisObject, dtFormat, dtSe
 function drawDatePicker(targetDateField, x, y)
 {
   var dt = getFieldDate(targetDateField.value );
- 
+
   // the datepicker table will be drawn inside of a <div> with an ID defined by the
   // global datePickerDivID variable. If such a div doesn't yet exist on the HTML
   // document we're working with, add one.
@@ -70,7 +70,7 @@ function drawDatePicker(targetDateField, x, y)
     newNode.setAttribute("style", "visibility: hidden;");
     document.body.appendChild(newNode);
   }
- 
+
   // move the datepicker div to the proper x,y coordinate and toggle the visiblity
   var pickerDiv = document.getElementById(datePickerDivID);
   pickerDiv.style.position = "absolute";
@@ -79,7 +79,7 @@ function drawDatePicker(targetDateField, x, y)
   pickerDiv.style.visibility = (pickerDiv.style.visibility == "visible" ? "hidden" : "visible");
   pickerDiv.style.display = (pickerDiv.style.display == "block" ? "none" : "block");
   pickerDiv.style.zIndex = 10000;
- 
+
   // draw the datepicker table
   refreshDatePicker(targetDateField.name, dt.getFullYear(), dt.getMonth(), dt.getDate());
 }
@@ -90,14 +90,14 @@ function refreshDatePicker(dateFieldName, year, month, day)
   // if no arguments are passed, use today's date; otherwise, month and year
   // are required (if a day is passed, it will be highlighted later)
   var thisDay = new Date();
- 
+
   if ((month >= 0) && (year > 0)) {
     thisDay = new Date(year, month, 1);
   } else {
     day = thisDay.getDate();
     thisDay.setDate(1);
   }
- 
+
   // the calendar will be drawn as a table
   // you can customize the table elements with a global CSS style sheet,
   // or by hardcoding style and formatting elements below
@@ -119,10 +119,10 @@ function refreshDatePicker(dateFieldName, year, month, day)
   var DIV_title = "<div class='dpTitleText'>";
   var DIV_selected = "<div class='dpDayHighlight'>";
   var xDIV = "</div>";
- 
+
   // start generating the code for the calendar table
   var html = TABLE;
- 
+
   // this is the title bar, which displays the month and the buttons to
   // go back to a previous month or forward to the next month
   html += TR_title;
@@ -130,45 +130,45 @@ function refreshDatePicker(dateFieldName, year, month, day)
   html += TD_title + DIV_title + monthArrayLong[ thisDay.getMonth()] + " " + thisDay.getFullYear() + xDIV + xTD;
   html += TD_buttons + getButtonCode(dateFieldName, thisDay, 1, "&gt;") + xTD;
   html += xTR;
- 
+
   // this is the row that indicates which day of the week we're on
   html += TR_days;
   for(i = 0; i < dayArrayShort.length; i++)
     html += TD_days + dayArrayShort[i] + xTD;
   html += xTR;
- 
+
   // now we'll start populating the table with days of the month
   html += TR;
- 
+
   // first, the leading blanks
   for (i = 0; i < thisDay.getDay(); i++)
     html += TD + "&nbsp;" + xTD;
- 
+
   // now, the days of the month
   do {
     dayNum = thisDay.getDate();
     TD_onclick = " onclick=\"updateDateField('" + dateFieldName + "', '" + getDateString(thisDay) + "');document.forms[0].submit();\">";
-    
+
     if (dayNum == day)
       html += TD_selected + TD_onclick + DIV_selected + dayNum + xDIV + xTD;
     else
       html += TD + TD_onclick + dayNum + xTD;
-    
+
     // if this is a Saturday, start a new row
     if (thisDay.getDay() == 6)
       html += xTR + TR;
-    
+
     // increment the day
     thisDay.setDate(thisDay.getDate() + 1);
   } while (thisDay.getDate() > 1)
- 
+
   // fill in any trailing blanks
   if (thisDay.getDay() > 0) {
     for (i = 6; i > thisDay.getDay(); i--)
       html += TD + "&nbsp;" + xTD;
   }
   html += xTR;
- 
+
   // add a button to allow the user to easily return to today, or close the calendar
   var today = new Date();
   var todayString = "Today is " + dayArrayMed[today.getDay()] + ", " + monthArrayMed[ today.getMonth()] + " " + today.getDate();
@@ -176,10 +176,10 @@ function refreshDatePicker(dateFieldName, year, month, day)
   html += "<button class='dpTodayButton' onClick='refreshDatePicker(\"" + dateFieldName + "\");'>this month</button> ";
   html += "<button class='dpTodayButton' onClick='updateDateField(\"" + dateFieldName + "\");'>close</button>";
   html += xTD + xTR;
- 
+
   // and finally, close the table
   html += xTABLE;
- 
+
   document.getElementById(datePickerDivID).innerHTML = html;
   // add an "iFrame shim" to allow the datepicker to display above selection lists
   adjustiFrame();
@@ -195,7 +195,7 @@ function getButtonCode(dateFieldName, dateVal, adjust, label)
     newMonth += 12;
     newYear += -1;
   }
- 
+
   return "<button class='dpButton' onClick='refreshDatePicker(\"" + dateFieldName + "\", " + newYear + ", " + newMonth + ");'>" + label + "</button>";
 }
 
@@ -207,7 +207,7 @@ function getDateString(dateVal)
   var monthString = "00" + (dateVal.getMonth()+1);
   dayString = dayString.substring(dayString.length - 2);
   monthString = monthString.substring(monthString.length - 2);
- 
+
   switch (dateFormat) {
     case "dmy" :
       return dayString + dateSeparator + monthString + dateSeparator + dateVal.getFullYear();
@@ -225,7 +225,7 @@ function getFieldDate(dateString)
   var dateVal;
   var dArray;
   var d, m, y;
- 
+
   try {
     dArray = splitDateString(dateString);
     if (dArray) {
@@ -256,7 +256,7 @@ function getFieldDate(dateString)
   } catch(e) {
     dateVal = new Date();
   }
- 
+
   return dateVal;
 }
 
@@ -274,7 +274,7 @@ function splitDateString(dateString)
     dArray = dateString.split("\\");
   else
     dArray = false;
- 
+
   return dArray;
 }
 
@@ -294,7 +294,7 @@ function datePickerClosed(dateField)
   var dateObj = getFieldDate(dateField.value);
   var today = new Date();
   today = new Date(today.getFullYear(), today.getMonth(), today.getDate());
- 
+
   if (dateField.name == "StartDate") {
     if (dateObj < today) {
       // if the date is before today, alert the user and display the datepicker again
@@ -317,14 +317,14 @@ function updateDateField(dateFieldName, dateString)
   var targetDateField = document.getElementsByName (dateFieldName).item(0);
   if (dateString)
     targetDateField.value = dateString;
- 
+
   var pickerDiv = document.getElementById(datePickerDivID);
   pickerDiv.style.visibility = "hidden";
   pickerDiv.style.display = "none";
- 
+
   adjustiFrame();
   targetDateField.focus();
- 
+
   // after the datepicker has closed, optionally run a user-defined function called
   // datePickerClosed, passing the field that was just updated as a parameter
   // (note that this will only run if the user actually selected a date from the datepicker)
@@ -341,7 +341,7 @@ function adjustiFrame(pickerDiv, iFrameDiv)
   var is_opera = (navigator.userAgent.toLowerCase().indexOf("opera") != -1);
   if (is_opera)
     return;
-  
+
   // put a try/catch block around the whole thing, just in case
   try {
     if (!document.getElementById(iFrameDivID)) {
@@ -355,12 +355,12 @@ function adjustiFrame(pickerDiv, iFrameDiv)
       newNode.setAttribute ("frameborder", "0");
       document.body.appendChild(newNode);
     }
-    
+
     if (!pickerDiv)
       pickerDiv = document.getElementById(datePickerDivID);
     if (!iFrameDiv)
       iFrameDiv = document.getElementById(iFrameDivID);
-    
+
     try {
       iFrameDiv.style.position = "absolute";
       iFrameDiv.style.width = pickerDiv.offsetWidth;
@@ -372,8 +372,8 @@ function adjustiFrame(pickerDiv, iFrameDiv)
       iFrameDiv.style.display = pickerDiv.style.display;
     } catch(e) {
     }
- 
+
   } catch (ee) {
   }
- 
+
 }
