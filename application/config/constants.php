09cd868dc50489745a3c7b5fdb68263841983fe0
Craig A Rodway <craig.rodway@gmail.com>
2022-01-13 21:59:34 +0000

    Bump

--- application/config/constants.php
+++ application/config/constants.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // Classroombookings version
 //
-define('VERSION', '2.6.0-beta6');
+define('VERSION', '2.6.0-beta7');
 
 // User auth types
 //
